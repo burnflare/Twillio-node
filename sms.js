cd9e9a4afcaafd9a628e253edc50fd42c80a355a
Vishnu Prem <vishnu@vishnuprem.com>
2013-10-21 02:22:48 +0800

    Updated text

--- sms.js
+++ sms.js
@@ -1,8 +1,8 @@
 var sms = module.exports;
 
-var part1 = "Hi, this is a reminder for you to complete your 10 minutes of daily meditation. Please remember to fill up the daily questionnaire - the link to the questionnaire is sent to your email. Your participant number is ";
-var part2 = ". This is Day ";
-var part3 = " of meditation"
+var part1 = "Hi, please remember to complete your daily meditation and fill up the daily questionnaire. The link is sent to your email. Your participant number is ";
+var part2 = ".";
+// var part3 = " of meditation"
 
 sms.send = function sms_send(req, res) {
 	res.json(200, {});
@@ -12,9 +12,9 @@ sms.send = function sms_send(req, res) {
 			var authToken = "42c9e857eb2e024f7bcd74ef64bd086f";
 			var client = require('twilio')(accountSid, authToken);
   
-  		  	console.log(part1 + sms.name + part2 + sms.day + part3);
+  		  	// console.log(part1 + sms.name + part2 + sms.day + part3);
 			client.sms.messages.create({
-			    body: part1 + sms.name + part2 + sms.day + part3,
+			    body: part1 + sms.name + part2,
 			    to: sms.number,
 			    from: "+1 720-408-2464"
 			}, function(err, message) {
