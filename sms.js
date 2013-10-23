ccf9aae62167fe841576a3c8df7d8c7dedb73a17
Vishnu Prem <vishnu@vishnuprem.com>
2013-10-23 18:40:01 +0800

    Updated errors

--- sms.js
+++ sms.js
@@ -10,7 +10,6 @@ sms.send = function sms_send(req, res) {
 	res.json(200, {});
 	var html = "Hey Vishnu & Brenda,<br />I've send an sms to the following people:<br />"
 	req.models.sms.find({ }, function (err, smss) {
-		console.log(err);
 		smss.forEach(function (sms, i) {
 			var accountSid = 'AC5c1eb49f29087d8b125fcd4d57e7d162';
 			var authToken = "42c9e857eb2e024f7bcd74ef64bd086f";
@@ -26,6 +25,7 @@ sms.send = function sms_send(req, res) {
   		  	console.log(sms.name + sms.number);
 			html += sms.name + " " + sms.number + "<br />";
 			if (i>=smss.length) {
+				console.log("Sending email");
 				send_email(html);
 			}
 		});
@@ -41,16 +41,16 @@ var transport = nodemailer.createTransport("SES", {
 function send_email(messages, fn) {
 	var payload = {
 	    from: "SMS Server <noreply@vishnuprem.com>",
-	    to: "vishnu@vishnuprem.com, scoblue@gmail.com",
+	    to: "vishnu@vishnuprem.com; scoblue@gmail.com",
 	    subject: "SMS Sent Receipt!",
 	    html: messages
 	}
 	
 	transport.sendMail(payload, function(error, response){
 	    if(error){
-	        fn("email could not be sent");
+	        console.log("email could not be sent" + error);
 	    }else{
-	        fn(null);
+	        console.log("Email sent!");
 	    }
 	    transport.close(); 
 	});
